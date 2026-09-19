82f4ad9faa0934ce3842f8adba93c5096a24ebca
Henry Bley <me@henrybley.com>
2026-09-19 16:08:37 +0200

    updated quickshell setup

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "Quickshell + Qt6 development environment";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -8,41 +9,43 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   outputs =
+    { self, nixpkgs, flake-utils, quickshell }:
     {
-      self,
-      nixpkgs,
-      flake-utils,
-      quickshell,
-    }:
-    {
-      homeManagerModules.default = import ./modules/home-manager.nix;
+      # System-independent, so it lives outside eachSystem
+      homeManagerModules.default =
+        { config, pkgs, ... }:
+        {
+          programs.quickshell = {
+            enable = true;
+            package = self.packages.${pkgs.stdenv.hostPlatform.system}.duckshell;
+            systemd.enable = true;
+            configs.default = config.lib.file.mkOutOfStoreSymlink
+              "${config.home.homeDirectory}/.config/duckshell";
+            activeConfig = "default";
+          };
+        };
     }
-    // flake-utils.lib.eachDefaultSystem (
+    // flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        qs = quickshell.packages.${system}.default;
       in
       {
-        packages.default = quickshell.packages.${system}.default;
-
-        packages.duckshell = pkgs.symlinkJoin {
-          name = "duckshell";
-          paths = [
-            quickshell.packages.${system}.default
-            pkgs.qt6.qtbase
-            pkgs.qt6.qtdeclarative
-            pkgs.qt6.qtwayland
-            pkgs.app2unit
-          ];
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = ''
-            wrapProgram $out/bin/quickshell \
-              --prefix QML_IMPORT_PATH : "${
-                quickshell.packages.${system}.default
-              }/lib/qt-6/qml:${pkgs.qt6.qtbase}/lib/qt-6/qml" \
-              --prefix QT_PLUGIN_PATH : "${pkgs.qt6.qtbase}/lib/qt-6/plugins"
-          '';
+        packages = rec {
+          duckshell = pkgs.symlinkJoin {
+            name = "duckshell";
+            paths = [ qs ];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            postBuild = ''
+              wrapProgram $out/bin/quickshell \
+                --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.app2unit ]}
+            '';
+            meta.mainProgram = "quickshell";
+          };
+          default = duckshell;
         };
 
         devShells.default = pkgs.mkShell {
@@ -51,12 +54,10 @@
             pkgs.qt6.qtbase
             pkgs.qt6.qtdeclarative
             pkgs.qt6.qtwayland
-            quickshell.packages.${system}.default
+            qs
           ];
           shellHook = ''
-            export QML_IMPORT_PATH=.:${
-              quickshell.packages.${system}.default
-            }/lib/qt-6/qml:${pkgs.qt6.qtbase}/lib/qt-6/qml
+            export QML_IMPORT_PATH=.:${qs}/lib/qt-6/qml:${pkgs.qt6.qtbase}/lib/qt-6/qml
             export QT_PLUGIN_PATH=${pkgs.qt6.qtbase}/lib/qt-6/plugins
             printf "%s\n" \
               "[General]" \
